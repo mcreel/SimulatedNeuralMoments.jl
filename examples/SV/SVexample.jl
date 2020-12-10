1b480b8b8cb497dbc2b3420db9526c641010be41
Michael Creel <michael.creel@uab.es>
2020-12-10 12:47:40 +0100

    add stochastic volatility example

--- examples/SV/SVexample.jl
+++ examples/SV/SVexample.jl
@@ -5,16 +5,16 @@ using BSON:@load
 # get the things to define the structure for the model
 # For your own models, you will need to supply the functions
 # found in MNlib.jl, using the same formats
-include("MNlib.jl")
+include("SVlib.jl")
 lb, ub = PriorSupport()
 
 # fill in the structure that defines the model
-model = SNMmodel("Mixture of Normals example model", lb, ub, InSupport, Prior, PriorDraw, auxstat)
+model = SNMmodel("Stochastic Volatility example", lb, ub, InSupport, Prior, PriorDraw, auxstat)
 
 # train the net, and save it and the transformation info
-nnmodel, nninfo = MakeNeuralMoments(model; Epochs=100)
+#nnmodel, nninfo = MakeNeuralMoments(model)
 #@save "neuralmodel.bson" nnmodel nninfo  # use this line to save the trained neural net 
-#@load "neuralmodel.bson" nnmodel nninfo # use this to load a trained net
+@load "neuralmodel.bson" nnmodel nninfo # use this to load a trained net
 
 # define the true parameter vector (either a specific design or a random value
 # θ = model.priordraw() # true parameter is a draw from prior
@@ -27,13 +27,12 @@ println("Basic NN estimation, true parameters (a draw from prior) and estimates"
 prettyprint([θ m], cnames)
 
 # draw a chain of length 10000, and get the extremum estimator
-chain, θhat = MCMC(m, 10000, model, nnmodel, nninfo, verbosity=true)
+chain, θhat = MCMC(m, 10000, model, nnmodel, nninfo, verbosity=false)
 
 # visualize results
-chn = Chains(chain, ["μ₁","μ₂","σ₁","σ₂","p"])
+chn = Chains(chain, ["ϕ", "ρ", "σ"])
 display(chn)
-plot(chn)
 println("SNM estimation, true parameters (a draw from prior) and extremum estimates")
 prettyprint([θ θhat], cnames)
-
+plot(chn)
 
