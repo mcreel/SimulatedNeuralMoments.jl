701fcc9684e201305b73c2b1b968ce353113ff05
Michael Creel <michael.creel@uab.es>
2021-07-14 14:57:26 +0200

    move DSGE to SNM

--- examples/DSGE/BuildMPI.jl
+++
@@ -1,3 +0,0 @@
-using Pkg
-Pkg.activate("./")
-ENV["JULIA_MPI_BINARY"]="system"; using Pkg; Pkg.build("MPI"; verbose=true) 
--- examples/DSGE/MCtable.jl
+++
@@ -1,8 +0,0 @@
-using SimulatedNeuralMoments, Statistics, DelimitedFiles
-results = readdlm("mcresults.txt")
-sofar = 500
-println("__________ replication: ", sofar, "_______________")
-clabels = ["99%", "95%", "90%"]
-prettyprint(reshape(mean(results[1:sofar,8:end],dims=1),7,3),clabels)
-dstats(results[1:sofar,1:7])
-
