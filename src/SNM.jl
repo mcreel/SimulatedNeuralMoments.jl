99469eb3eaad05667cc27019cea2ad4778112920
Michael Creel <michael.creel@uab.es>
2021-09-04 11:47:09 +0200

    bounds enforecement for covariance

--- src/SNM.jl
+++ src/SNM.jl
@@ -14,20 +14,20 @@ function NeuralMoments(θ, reps, model::SNMmodel, nnmodel, nninfo)
 end        
 # neural moments given statistic
 function NeuralMoments(z, model::SNMmodel, nnmodel, nninfo)
-    min.(max.(Float64.(nnmodel(TransformStats((z[:])', nninfo)')), model.lb), model.ub)
+    Float64.(nnmodel(TransformStats((z[:])', nninfo)'))
 end        
 
 # estimate covariance
 function EstimateΣ(θ, reps, model::SNMmodel, nnmodel, nninfo)
     z = model.auxstat(θ, reps) 
-    cov([NeuralMoments(z[i], model, nnmodel, nninfo) for i = 1:reps])
+    cov([min.(max.(NeuralMoments(z[i], model, nnmodel, nninfo), model.lb), model.ub) for i = 1:reps])
 end
 
 # moments and covariance
 function mΣ(θ, reps, model::SNMmodel, nnmodel, nninfo)
     z = model.auxstat(θ, reps) 
     Zs = [NeuralMoments(z[i], model, nnmodel, nninfo) for i = 1:reps]
-    mean(Zs), cov(Zs)
+    mean(Zs), cov(min.(max.(Zs,model.lb),model.ub))
 end
     
 # method with identity weight
