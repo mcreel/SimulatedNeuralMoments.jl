f5c8ac6224001ea3dabf36e58ae3072dfc660be5
Michael Creel <michael.creel@uab.es>
2022-02-13 17:20:26 +0100

    getting closer

--- examples/SV/SVexample.jl
+++ examples/SV/SVexample.jl
@@ -19,6 +19,7 @@ model = SNMmodel("Stochastic Volatility example", lb, ub, InSupport, Prior, Prio
 # draw a sample at the design parameters, or use an existing data set
 #y = SVmodel(TrueParameters()) # draw a sample of 500 obsns. at design parameters
 y = readdlm("svdata.txt") # load a data set
+n = size(y,1)
 p1 = plot(y)
 p2 = density(y)
 plot(p1, p2, layout=(2,1))
@@ -26,18 +27,39 @@ plot(p1, p2, layout=(2,1))
 
 # define the neural moments using the real data
 m = NeuralMoments(auxstat(y), model, nnmodel, nninfo)
+m = m[:]
 @show m
-## draw a chain of length 10000 plus 500 burnin
-chain, junk, junk = MCMC(m, 10500, model, nnmodel, nninfo, do_cue=true, verbosity=true)
-chain = chain[501:end,:]
-# visualize results
-chn = Chains(chain, ["ϕ", "ρ", "σ"])
-display(chn)
-println("SNM estimation, estimated pos. median")
-cnames = ["pos. median"] 
-prettyprint(median(chain,dims=1)[:], cnames)
-display(plot(chn))
-#savefig("chain.png")
-#writedlm("chain.txt", chain)
+
+S = 200
+covreps = 1000
+length = 1000
+nchains = 3
+burnin = 1000
+tuning = 2*sqrt(500)
+
+@model function MSM(m, S, model)
+    # create the prior: the product of the following array of marginal priors
+    θ  ~ arraydist([Uniform(model.lb[i], model.ub[i]) for i = 1:size(model.lb,1)])
+    if !InSupport(θ)
+        Turing.@addlogprob! -Inf
+        # Exit the model evaluation early
+        return
+    end    # sample from the model, at the trial parameter value, and compute statistics
+    mbar, Σ = SimulatedNeuralMoments.mΣ(θ, S, model, nnmodel, nninfo)
+    mbar = vec(mbar)
+    m ~ MvNormal(mbar, Σ)
 end
-main()
+
+# get covariance of proposal
+Σ = EstimateΣ(m, covreps, model, nnmodel, nninfo) 
+proposal = MH(:θ => AdvancedMH.RandomWalkProposal(MvNormal(zeros(3), tuning.*Σ)))
+chain = sample(MSM(m, S, model), init_params=m, proposal,
+        MCMCThreads(), length+burnin, nchains; param_names=["α","ρ","σ"])
+chain = chain[burnin+1:end,:,:]
+end
+chain = main()
+display(chain)
+display(plot(chain))
+chain = Array(chain)
+acceptance = size(unique(chain[:,1]),1)[1] / size(chain,1)
+println("acceptance rate: $acceptance")
