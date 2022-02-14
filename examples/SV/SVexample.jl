2817d471691256013141e0fcf15011b0214719fe
Michael Creel <michael.creel@uab.es>
2022-02-14 13:13:35 +0100

    ss

--- examples/SV/SVexample.jl
+++ examples/SV/SVexample.jl
@@ -50,9 +50,10 @@ junk, Σp = mΣ(θinit, covreps, model, nnmodel, nninfo)
         return
     end    
     # sample from the model, at the trial parameter value, and compute statistics
-    mbar, Σ = TmΣ(θ, S, model, nnmodel, nninfo)
+    mbar, Σ, Zs = TmΣ(θ, S, model, nnmodel, nninfo)
     @show θ 
     @show Σ
+    @show Zs
     m ~ MvNormal(mbar, Σ)
 end
 
