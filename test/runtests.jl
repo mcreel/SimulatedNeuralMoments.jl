600d9fef1f3784361cb550d91768908629e49b08
Michael Creel <michael.creel@uab.es>
2022-06-18 11:21:49 +0200

    begin adding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using SimulatedNeuralMoments, Flux, Test
 using BSON:@load
-
+cd(@__DIR__)
 @testset "SimulatedNeuralMoments.jl" begin
-    BSON.@load "neuralmodel.bson" nnmodel nninfo
+    @load "neuralmodel.bson" nnmodel nninfo
     z = zeros(11)
     @test NeuralMoments(zeros(11), nnmodel, nninfo)[1] ≈ -5.850073134957904 atol = 1e-5
 end
