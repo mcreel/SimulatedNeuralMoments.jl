6138ea986a3c2152c1760c790ec97592a47432dd
Michael Creel <michael.creel@uab.es>
2022-07-16 06:50:55 +0200

    fix importance plot for new flux

--- src/Importance.jl
+++ src/Importance.jl
@@ -3,7 +3,7 @@ using Flux
 using StatsPlots
 # get the first layer parameters for influence analysis
 @load "neuralmodel.bson" nnmodel
-beta = nnmodel.layers[1].W # get first layer betas
+beta = nnmodel.layers[1].weight # get first layer betas
 z = maximum(abs.(beta),dims=1);
 heatmap(z, xlabel="statistic", title="Importance of inputs, bright=high, dark=low")
 savefig("ImportanceOfStatistics.png")
