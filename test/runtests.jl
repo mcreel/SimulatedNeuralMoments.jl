310d674e905e6d7c2352afeaa65fed95deafee70
Michael Creel <michael.creel@uab.es>
2022-08-21 08:15:57 -0700

    add more tests, check 1.8 compatibility, prepare v1.0.3 release

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,16 @@
-using SimulatedNeuralMoments, Flux, Test
+using SimulatedNeuralMoments, Flux, Test, LinearAlgebra
 using BSON:@load
 cd(@__DIR__)
 @testset "SimulatedNeuralMoments.jl" begin
-    @load "neuralmodel.bson" nnmodel nninfo
+    @load "../examples/SV/neuralmodel.bson" nnmodel nninfo
+    include("../examples/SV/SVlib.jl")
     z = zeros(11)
-    @test NeuralMoments(zeros(11), nnmodel, nninfo)[1] ≈ -5.850073134957904 atol = 1e-5
+   lb, ub = PriorSupport() # bounds of support
+    model = SNMmodel("Stochastic Volatility example", lb, ub, InSupport, PriorDraw, auxstat)
+    m, Σp = mΣ((lb+ub)./2.0, 100, model, nnmodel, nninfo)
+    @test NeuralMoments(zeros(11), nnmodel, nninfo)[1] ≈ -5.85007 atol = 1e-5
+    @test isposdef(Σp)
+    @test ishermitian(Σp)
+    @test size(Σp,1) == 3
+    @test size(m,1) == 3
 end
