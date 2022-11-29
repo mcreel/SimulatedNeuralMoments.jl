fcca3a6c27e1c45d1fa2618cebc5778e7ce2ba18
Michael Creel <michael.creel@uab.es>
2022-11-29 11:28:17 +0100

    working on tests

--- examples/SV/SVexample.jl
+++ examples/SV/SVexample.jl
@@ -75,10 +75,9 @@ for i = 1:size(chain,1)
     chain[i,:] = invlink(@Prior, chain[i,:])
 end
 chain = Chains(chain, names)
-return chain, mbar, Σp
-end
-chain, mbar, Σp = main()
-
 display(chain)
 display(plot(chain))
+return chain, mbar, Σp
+end
+
 #savefig("chain.png")
