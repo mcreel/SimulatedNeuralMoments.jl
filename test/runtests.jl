b72ad2f6a904560626b19f706ef43cb21ec959f0
Michael Creel <michael.creel@uab.es>
2022-11-29 10:17:10 +0100

    add example deps to package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,7 @@
 #using SimulatedNeuralMoments, 
 using Flux, Test, LinearAlgebra
 using BSON:@load
-cd(@__DIR__)
+
 @testset "SimulatedNeuralMoments.jl" begin
-    @load "neuralmodel.bson" nnmodel nninfo
-    include("SVlib.jl")
-    z = zeros(11)
-    lb, ub = PriorSupport() # bounds of support
-    model = SNMmodel("Stochastic Volatility example", lb, ub, InSupport, PriorDraw, auxstat)
-    m, Σp = mΣ((lb+ub)./2.0, 100, model, nnmodel, nninfo)
-    @test size(NeuralMoments(zeros(11), nnmodel, nninfo),1) == 3
-    @test isposdef(Σp)
-    @test ishermitian(Σp)
-    @test size(Σp,1) == 3
-    @test size(m,1) == 3
+    include("../examples/SV/SVexample.jl")
 end
