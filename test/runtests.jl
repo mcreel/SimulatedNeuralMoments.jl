301d5e33575293f38e75703030f1e826884bc02a
Michael Creel <michael.creel@uab.es>
2023-07-11 12:09:05 +0200

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,21 @@
 #using SimulatedNeuralMoments, 
 using Flux, Test, LinearAlgebra, Statistics
 using BSON:@load
+cd(@__DIR__)
 
-@testset "SimulatedNeuralMoments.jl" begin
+function main()
+@testset "SV" begin
     include("../examples/SV/SVexample.jl")
-    chain, θhat, Σp = SVexample(5000, 200) # fast run
+    chain, θhat, Σp = SVexample(5000, 100) # fast run
     @test size(θhat,1) == 3
     @test size(Σp) == (3,3)
 end
+
+@testset "MN" begin
+    include("../examples/MN/MNexample.jl")
+    chain, θhat, Σp = MNexample(5000, 100) # fast run
+    @test size(θhat,1) == 5
+    @test size(Σp) == (5,5)
+end
+end
+main()
