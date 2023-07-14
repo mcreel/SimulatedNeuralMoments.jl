64a3af54939d059c96c9d24baa1b88d5bdb19068
Michael Creel <michael.creel@uab.es>
2023-07-14 16:12:17 +0200

    fix path in test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ cd(@__DIR__)
 @testset "SimulatedNeuralMoments" begin
     
     @info "running MN example with a small sample"
-    include("../example/MNexample.jl")
+    include("../examples/MNexample.jl")
     acceptance, rmse = runme(10000,100,false) # fast run
     @test rmse < 0.2
     @test acceptance > 0.1
     @test acceptance < 0.5
 
     @info "running MN example with a small sample"
-    include("../example/SVexample.jl")
+    include("../examples/SVexample.jl")
     acceptance, rmse = runme(10000,100,false) # fast run
     @test rmse < 0.2
     @test acceptance > 0.1
