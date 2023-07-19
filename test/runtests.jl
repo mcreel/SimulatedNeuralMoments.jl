7b98ae82ecaa104d7a7911f18d770bbbfc59166c
Michael Creel <michael.creel@uab.es>
2023-07-19 15:20:30 +0200

    use short run for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ cd(@__DIR__)
 @testset "SimulatedNeuralMoments" begin
     
     @info "running SV example with a small sample"
-    testmode = ("SVlib.jl", 10000, 100, false)
+    global testmode = ("SVlib.jl", 10000, 100, false)
     include("../examples/Example.jl")
     @test rmse < 0.2
     @test acceptance > 0.1
     @test acceptance < 0.5
 
     @info "running MN example with a small sample"
-    testmode = ("MNlib.jl", 10000, 100, false)
+    global testmode = ("MNlib.jl", 10000, 100, false)
     include("../examples/Example.jl")
     @test rmse < 0.2
     @test acceptance > 0.1
